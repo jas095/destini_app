d85f69284a12d9110ed747a8fdf28bda32208c98
Jose Angel <jas@deb>
2020-08-12 15:48:38 -0400

    Completed Project

--- lib/story.dart
+++ lib/story.dart
@@ -1,9 +1,9 @@
 class Story {
   String storyTitle, choice1, choice2;
 
-  Story({String storyT, String c1, String c2}) {
-    storyTitle = storyT;
-    choice1 = c1;
-    choice2 = c2;
+  Story({String storyTitle, String choice1, String choice2}) {
+    this.storyTitle = storyTitle;
+    this.choice1 = choice1;
+    this.choice2 = choice2;
   }
 }
